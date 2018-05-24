f9222452be027c237f23bdba296f5a74c79fe254
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-24 08:22:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
+  attr_accessor :item :price :discount :last_item#:cash_register :cash_register_with_discount
 
+#how do we create 2 types of cash registers? do instance methods get moved to inside these methods? or called outside this initialization method inside class?
+  def initialize(discount = 0)
+    @total = 0
+    @items = []
+    @discount = discount
+  end
+
+  def self.total
+    @total
+  end
+
+  def self.items
+    @items
+  end
+
+  def self.add_item(item, price, quantity=1)
+    @items << item
+    @total += (price * quantity)
+    #keep track of last item in case it needs to be voided
+    last_item = [item, price, quantity]
+  end
+
+#how to apply discount methods for both types of cash register??
+  def self.cash_register::apply_discount
+    puts "There is no discount to apply."
+  end
+
+  def self.cash_register_with_discount::apply_discount
+    @total -= (@total * discount)
+    puts "After the discount, the total comes to #{@total}"
+  end
+
+#delete last added item and subtract price from total- how to find this item?
+  def void_last_transaction
+    @items.last.delete
+    @total -= @items.last.price
+end
